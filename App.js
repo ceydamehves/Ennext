c9f5be26357cb09a1e51fe883a5df405d72d4ab4
ceydamehves <cmehves.keklik@std.hku.edu.tr>
2019-12-21 16:14:25 +0300

    App Works Correctly

---
+++ App.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { StyleSheet, Text, View ,Image} from 'react-native';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import {WebView} from 'react-native-webview';
+
+class SplashScreen extends React.Component {
+  
+  render() {
+
+    return (
+      
+      <View style={[styles.container, { backgroundColor: 'gray' }]}>
+        <Image source={require ('C:/Ennext/assets/logo.jpg')}
+               style={{ width:wp('96%'),height:hp('100%') }}/>
+      </View> 
+    );
+  }
+}
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { isLoading: true }
+  }
+
+  performTimeConsumingTask = async() => {
+    return new Promise((resolve) =>
+      setTimeout(
+        () => { resolve('result') },
+        3000
+      )
+    );
+  }
+
+  async componentDidMount() {
+    const data = await this.performTimeConsumingTask();
+
+    if (data !== null) {
+      this.setState({ isLoading: false });
+    }
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return <SplashScreen />;
+    }
+
+    return (
+      <WebView
+        source={{ uri: 'https://ennexthzq.com/' }}
+        style={{ marginTop: 2 }}
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  }
+  
+});
